823f89bb6ea39650865a92898591321298a428a3
BostonSlettum22 <816661@mystma.org>
2021-03-08 16:33:27 +0000

    final 1

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-#WRITE YOUR CODE IN THIS FILE
+def times10(x):
+    return 10 * x
+
+print(times10(2))
+
+
+def firstA(w):
+
+    if w[0] == "a":
+        return True
+    
+    return False
+
+print(firstA("android"))
+
+
+
+
+
+
