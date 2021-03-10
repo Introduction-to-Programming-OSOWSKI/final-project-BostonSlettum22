b1e95ad8f364a6732ea7eaca7073922f9d2869f0
BostonSlettum22 <816661@mystma.org>
2021-03-10 16:21:53 +0000

    final 2

--- main.py
+++ main.py
@@ -15,6 +15,27 @@ print(firstA("android"))
 
 
 
+def sizer(n):
 
+    if n < 10:
+        return "small"
+    if n < 100:
+        return "medium"
+    if n > 100:
+        return "large"
+
+print(sizer(2))
+
+
+def doubleword(w):
+
+    word = w
+
+    for i in range(1,len(w)):
+        word = word + word
+
+    return word
+
+print(doubleword("go"))
 
 
